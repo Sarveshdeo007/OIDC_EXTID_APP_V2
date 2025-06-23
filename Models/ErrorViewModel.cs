ca438ed82af19080a2e83d92807011c42c117263
Sarvesh <sarveshdeogupta007@gmail.com>
2025-06-23 19:18:36 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OIDC_EXTID_APP_V2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
